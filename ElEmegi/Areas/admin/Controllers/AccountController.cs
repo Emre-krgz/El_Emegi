285070fbb4bb6965f5d69123d25fdf1bdb2d3d22
Karagöz\Ramazan <ramazan-emre93@hotmail.com>
2016-12-22 11:19:36 +0200

    Admin Paneli Giriş Sayfası

---
+++ ElEmegi/Areas/admin/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using ElEmegi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ElEmegi.Areas.admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /admin/Account/
+
+        ElEmegi2Context db = new ElEmegi2Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection form)
+        {
+            string username = form["username"].ToString();
+            string password = form["password"].ToString();
+            var usr = (from u in db.musterilers
+                       where u.kullanici_adi == username && u.sifre == password
+                       select u).FirstOrDefault();
+
+            if (usr != null)
+            {
+                FormsAuthentication.SetAuthCookie(usr.kullanici_adi, false);
+                return RedirectToAction("Index", "AdminHome");
+            }
+            TempData["Message"] = "Kullanıcı ve Şifre Yanlış";
+            return View();
+
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Account", "admin");
+        }
+
+    }
+}
